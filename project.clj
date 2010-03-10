715487bc0c0ca57bc747003772ab99f8bd7fc13a
ulf <boegeholz@gmail.com>
2010-03-10 23:51:24 +0100

    Update dependency version for leiningen swank

--- project.clj
+++ project.clj
@@ -6,4 +6,4 @@
   :dependencies     [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]
 		     [org.clojure/clojure-contrib "1.1.0-new-SNAPSHOT"]
 		     [compojure "0.3.2"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
